a912118b252e8447e4d8249d2373072f3c071ddf
AnshCoderRepo <anshadarsh420@gmail.com>
2025-07-25 18:26:45 +0530

    messeg

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/raect-animation/',
   plugins: [react()],
   server: {
     host: true,
